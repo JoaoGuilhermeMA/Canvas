f64cb3be73a62e44e2cc44bc835aa5adafd107e6
Joao Guilherme <106107863+JoaoGuilhermeMA@users.noreply.github.com>
2023-10-26 09:26:26 -0300

    treinando js na faculdade

---
+++ script.js
@@ -0,0 +1,46 @@
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+
+// ctx.fillStyle = 'red'
+// ctx.fillRect(0,0,100,100);
+// ctx.lineWidth = 3;
+// ctx.strokeStyle = 'blue';
+// ctx.strokeRect(100,100, 100,100);
+// ctx.moveTo(200, 200);
+// ctx.lineTo(300, 300);
+
+// ctx.beginPath();
+// ctx.arc(250, 50, 40, 0, 2*Math.PI);
+// ctx.fill();
+// ctx.stroke();
+
+// let imagem = new Image();
+// imagem.src = 'NicePng_sprite-sheet-png_2303116.png';
+// let l = 437;
+// let a = 563;
+
+// imagem.onload = function() {
+//     ctx.drawImage(imagem, 0, 0, 300, 300);
+//     ctx.drawImage(imagem, 0, 0, l,a, 300, 300, 100, 100);
+//     ctx.drawImage(imagem, l, 0, l+50,a, 400, 400, 100, 100);
+//     ctx.drawImage(imagem, l*2+50, 0, l,a, 500, 500, 100, 100);
+//     ctx.drawImage(imagem, l*2*2-360, 0, l,a, 600, 600, 100, 100);
+// }
+
+/*
+drawImage(imagem, xOrigem, yOrigem, larguraOrigem,
+alturaOrigem, xDestino, yDestino, larguraDestino,
+alturaDestino): desenha parte da imagem.
+ */
+
+// ctx.fillStyle = 'green';
+// ctx.fillRect(50, 50, 25, 25);
+// ctx.save();
+// ctx.fillStyle = 'purple';
+// ctx.fillRect(100, 50, 25, 25);
+// ctx.save();
+// ctx.fillStyle = 'blue';
+// ctx.fillRect(150, 50, 25, 25);
+// ctx.save();
+// ctx.restore();
+
